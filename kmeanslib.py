bcfda5ea6319758223b37a1e7d20a8a89931b177
martisu <martisurralles@hotmail.com>
2024-02-28 20:29:01 +0100

    Classe cluster creada

--- kmeanslib.py
+++ kmeanslib.py
@@ -1,4 +1,5 @@
 from ctypes import *
+
 lib_path = '/home/alumnos/ppia/ppia-12/Escritorio/prac1/kmeanslib.so'
 lib = CDLL(lib_path)
 
@@ -6,5 +7,13 @@ lib = CDLL(lib_path)
 say_hello = lib.say_hello
 say_hello(b"Martisu")
 
-class cluster(): #??
-    pass
+class Cluster(Structure): 
+    _fields_=[("num_puntos", c_uint32),
+              ("r", c_uint8),
+              ("g", c_uint8),
+              ("b", c_uint8),
+              ("media_r", c_uint32),
+              ("media_g", c_uint32),
+              ("media_b", c_uint32)]
+    
+    
