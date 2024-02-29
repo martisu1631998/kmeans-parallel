2b6d6ba3b0f4d0a3c5fd58737b92685189fd9bf0
martisu <martisurralles@hotmail.com>
2024-02-29 17:15:03 +0100

    Structs traduides uwu

--- kmeanslib.py
+++ kmeanslib.py
@@ -7,6 +7,7 @@ lib = CDLL(lib_path)
 say_hello = lib.say_hello
 say_hello(b"Martisu")
 
+# Structure translations
 class Cluster(Structure): 
     _fields_=[("num_puntos", c_uint32),
               ("r", c_uint8),
@@ -15,5 +16,16 @@ class Cluster(Structure):
               ("media_r", c_uint32),
               ("media_g", c_uint32),
               ("media_b", c_uint32)]
+
+class Pixel(Structure): # rgb
+    _fields_=[("b", c_uint8), ("g", c_uint8), ("r", c_uint8)]
+
+class Image(Structure):
+    _fields_=[("length", c_uint32),
+               ("width", c_uint32),
+               ("height", c_uint32),
+               ("header", c_uint8*54),
+               ("pixels", POINTER(Pixel)),
+               ("fp", c_void_p)] # c_void_p acts as a placeholder because c_types doesn't have an equivalent
     
-    
+
