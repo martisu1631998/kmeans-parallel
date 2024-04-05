d98d00bb1ddc4b16d3ae0653736711c88779b427
martisu <marti.surralles@autonoma.cat>
2024-04-05 19:06:33 +0200

    solucio per poder fer les reductions?

--- kmeanslib_parallel.c
+++ kmeanslib_parallel.c
@@ -224,14 +224,38 @@ void kmeans(uint8_t k, cluster* centroides, uint32_t num_pixels, rgb* pixels){
 		}
    
 		// Find closest cluster for each pixel
-		for(j = 0; j < num_pixels; j++) 
+		/*for(j = 0; j < num_pixels; j++) 
     	{
 			closest = find_closest_centroid(&pixels[j], centroides, k);
 			centroides[closest].media_r += pixels[j].r;
 			centroides[closest].media_g += pixels[j].g;
 			centroides[closest].media_b += pixels[j].b;
 			centroides[closest].num_puntos++;
+		}*/
+
+		
+		// Find closest cluster for each pixel
+		int closest_by_pixel[num_pixels]
+		#pragma omp parallel{
+
+		#pragma omp for
+		for (j=0; j<num_pixels; j++){
+			closest_by_pixel[j] = find_closest_centroid(&pixels[j], centroides, k);
 		}
+
+		int means[3][k]
+		for (int color; color<3; color++){
+			#pragma omp for reduction(+:means[color])
+			for (j=0; j<num_pixels; j++){
+				closest = cosest_by_pixel[j]
+				means[color][closest] += pixels[j].r;
+			}
+		}
+		// Després d'això només cal afegir tots els valors a centroides.
+		//També falta afegir quants pixels hi ha de cada a centroids cosa que es pot obtenir de closest_by_pixel
+		
+		}
+		
 
 		// Update centroids & check stop condition
 		condition = 0;
