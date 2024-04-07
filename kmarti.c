b22ef5322271e514dde3db7fcca13d0385a41b28
martisu <marti.surralles@autonoma.cat>
2024-04-07 15:09:23 +0200

    nose tio

--- kmarti.c
+++ kmarti.c
@@ -215,7 +215,7 @@ void kmeans(uint8_t k, cluster* centroides, uint32_t num_pixels, rgb* pixels){
 	do 
   	{
 		// Reset centroids
-		printf("STEP 3.0: Reset centroids");
+		printf("STEP 3.0: Resetting centroids");
 		for(j = 0; j < k; j++) 
     	{
 			centroides[j].media_r = 0;
@@ -233,34 +233,31 @@ void kmeans(uint8_t k, cluster* centroides, uint32_t num_pixels, rgb* pixels){
 			centroides[closest].media_b += pixels[j].b;
 			centroides[closest].num_puntos++;
 		}*/
- 
+
 		
 		// Find closest cluster for each pixel
-		int closest_by_pixel[num_pixels];		
-		printf("STEP 3.1: Finding closest centroids");
-		// #pragma omp parallel for
+		// int closest_by_pixel[num_pixels]
+		// #pragma omp parallel
+		// {
+		// #pragma omp for
 		for (j=0; j<num_pixels; j++){
 			closest_by_pixel[j] = find_closest_centroid(&pixels[j], centroides, k);
-			centroides[closest_by_pixel[j]].num_puntos++; // Comptar aquí dins lo dels pixels a cada centroide
+			centroides[closest_by_pixel[j]].num_puntos++;
 		}
-		printf("STEP 3.2: Updating centroid means");
-		int means[k][3];		
-		// #pragma omp parallel for reduction(+:means[closest])
-		for (j=0; j<num_pixels; j++){
-			closest = closest_by_pixel[j];
-			means[closest][0] += pixels[j].r;
-			means[closest][1] += pixels[j].g;
-			means[closest][2] += pixels[j].b;
-		}
-		
-		for (int x=0; x<k; x++){			
-				centroides[x].media_r = means[x][0];
-				centroides[x].media_g = means[x][1];
-				centroides[x].media_b = means[x][2];
-			}
-		
-		// Després d'això només cal afegir tots els valors a centroides.
 
+		int means[3][k];
+		for (int color; color<3; color++){
+			// #pragma omp for reduction(+:means[color])
+			for (j=0; j<num_pixels; j++){
+				closest = cosest_by_pixel[j]
+				means[color][closest] += pixels[j].r;
+			}
+		}
+		// Després d'això només cal afegir tots els valors a centroides.
+		//També falta afegir quants pixels hi ha de cada a centroids cosa que es pot obtenir de closest_by_pixel
+		
+		// }
+		
 
 		// Update centroids & check stop condition
 		condition = 0;
